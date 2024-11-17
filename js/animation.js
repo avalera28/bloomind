cc6c38b7a087ec08f36485f22afc7aa5a3dbb80c
Ana Valera <avalera28@gmail.com>
2024-11-17 21:15:53 +0100

    improve animation home

--- js/animation.js
+++ js/animation.js
@@ -1,10 +1,16 @@
+
 document.addEventListener("DOMContentLoaded", function () {
     gsap.registerPlugin(DrawSVGPlugin);
   
     // Anima todos los trazos de los paths en el SVG
     gsap.fromTo(
-      "path", // Asegúrate de que este selector coincida con los trazos del SVG
+      ".home_logo .b", // Asegúrate de que este selector coincida con los trazos del SVG
       { drawSVG: "0%" },
-      { drawSVG: "100%", duration: 3, ease: "power1.inOut", stagger: 0.3 }
+      { drawSVG: "100%", duration: 3, ease: "power1.inOut", stagger: 0.3, fill: "#da483b"}
     );
+    gsap.fromTo(
+        ".home_logo .c", // Asegúrate de que este selector coincida con los trazos del SVG
+        { drawSVG: "0%" },
+        { drawSVG: "100%", duration: 3, ease: "power1.inOut", stagger: 0.3, fill: "#0c0c0c"}
+      );
   });
